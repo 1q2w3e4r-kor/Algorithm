e0f720d3141d7b8a004f7dba3d39a2661c651138
1q2w3e4r-kor <dldudco2@gmail.com>
2021-08-03 21:57:36 +0900

    Update Solution.java

--- Heap/More_Spicy/src/Solution.java
+++ Heap/More_Spicy/src/Solution.java
@@ -1,9 +1,26 @@
+import java.util.Collections;
+import java.util.PriorityQueue;
 
 public class Solution {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[] scoville = {1, 2, 3, 9, 10, 12};
+		System.out.println(solution(scoville,7));
 	}
-
+	
+	public static int solution(int[] scoville, int K) {
+        int answer = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int a:scoville) pq.add(a);
+        while(pq.peek()<=K) {
+        	int first = pq.poll();
+        	int second = pq.poll();
+        	pq.add(first+second*2);
+        	answer++;
+        	System.out.println(first+" "+second+" "+(first+second*2));
+        }
+        while(!pq.isEmpty()) System.out.print(pq.poll()+" ");
+        System.out.println();
+        return answer;
+    }
 }
