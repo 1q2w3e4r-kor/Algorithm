5aa234a50772a72a6b8871f20ff08d73cec7af42
1q2w3e4r-kor <dldudco2@gmail.com>
2021-08-10 15:19:56 +0900

    Searching_Prime_Number

---
+++ Full_Search/Searching_Prime_Number/src/Solution.java
@@ -0,0 +1,24 @@
+public class Solution {
+
+	public static void main(String[] args) {
+
+		String numbers = "011";
+		System.out.print(solution(numbers));
+
+	}
+
+	public static int solution(String numbers) {
+		int answer = 0;
+		String[] arr = numbers.split("");
+		answer=prime(arr);
+        return answer;
+	}
+	
+	public static int prime(String[] arr) {
+		int cnt=0;
+		for(int i=0;i<arr.length;i++) {
+			arr[]
+		}
+		return cnt;
+	}
+}
