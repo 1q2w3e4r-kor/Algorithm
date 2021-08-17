f11674e98628f269acaeb6303d11a6ab407370d8
1q2w3e4r-kor <72718608+1q2w3e4r-kor@users.noreply.github.com>
2021-08-17 23:20:15 +0900

    Network

---
+++ DFS,BFS/BFS/Network/src/Solution.java
@@ -0,0 +1,19 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		int n=3;
+		int[][] computers= {{1, 1, 0}, {1, 1, 0}, {0, 0, 1}};
+		System.out.println(solution(n,computers));
+	}
+	
+	public static int solution(int n, int[][] computers) {
+        int answer = 0;
+        Queue<Integer> queue = new LinkedList<>();
+        
+        return answer;
+    }
+
+}
